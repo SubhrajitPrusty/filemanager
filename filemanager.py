a03bd304cfeb801a1fab4caa218c568e518daec6
Subhrajit Prusty <subhrajit1997@gmail.com>
2017-10-10 23:44:18 +0530

    Upload files

---
+++ filemanager.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+
+import os
+
+
+def filenamefixer(filename):
+	if "'" in filename:
+		filename.replace("'","\'")
+	return filename
+
+
+if os.name == "nt":
+	files = os.popen("dir /b /a-d").read().split("\n")
+elif os.name == "posix":
+	files = os.popen("find -maxdepth 1").read().split("\n")
+
+files.remove("filemanager.py")
+ext = set([x.split(".")[::-1][0] for x in files if "." in x])
+extensions = [x for x in ext if not x.isupper() and len(x) !=0 and not x.isnumeric()]
+
+for ex in extensions:
+	os.system("mkdir "+ex)
+	thisFiles = [f for f in files if f.endswith(ex)]
+
+	for f in thisFiles:
+		f = filenamefixer(f)
+		if os.name == "nt":
+			move = 'move "{}" {}'.format(f,ex)
+		elif os.name == "posix":
+			move = "mv '{}' ./{}".format(f,ex)
+		os.system(move)
+
+
